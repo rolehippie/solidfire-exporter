36998dea4c8bab4e891f22d84c4a59ddd3d90374
Thomas Boerger <thomas@webhippie.de>
2023-01-05 23:30:53 +0100

    feat: restructure workflows and enable automated releases

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,20 @@
+import os
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
+
+
+def test_dummy(host):
+    assert True
+
+
+# def test_running_and_enabled(host):
+#     svc = host.service("solidfire-exporter")
+#     assert svc.is_running
+#     assert svc.is_enabled
+
+
+# def test_server_socket(host):
+#     assert host.socket("tcp://127.0.0.1:9987").is_listening
