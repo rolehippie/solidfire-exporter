b9407ee939cc6753b79b9a449125ed9b3fa5a4f3
Thomas Boerger <thomas@webhippie.de>
2025-10-18 14:51:39 +0200

    feat: apply new repo structure and update linting

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# workspace
+# solidfire-exporter
 
 [![Source Code](https://img.shields.io/badge/github-source%20code-blue?logo=github&logoColor=white)](https://github.com/rolehippie/solidfire-exporter)
 [![General Workflow](https://github.com/rolehippie/solidfire-exporter/actions/workflows/general.yml/badge.svg)](https://github.com/rolehippie/solidfire-exporter/actions/workflows/general.yml)
--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,15 +6,11 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-def test_dummy(host):
-    assert True
+def test_running_and_enabled(host):
+    svc = host.service("solidfire-exporter")
+    assert svc.is_running
+    assert svc.is_enabled
 
 
-# def test_running_and_enabled(host):
-#     svc = host.service("solidfire-exporter")
-#     assert svc.is_running
-#     assert svc.is_enabled
-
-
-# def test_server_socket(host):
-#     assert host.socket("tcp://127.0.0.1:9987").is_listening
+def test_server_socket(host):
+    assert host.socket("tcp://127.0.0.1:9987").is_listening
